e2e7a0c92c960397ccd72d143beea80f796ded2c
GuhankarthickC <guhankarthick.c@gmail.com>
2022-07-07 17:26:03 +0530

    Admin DashBoard

--- Admin/src/app/app.module.ts
+++ Admin/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { CustomersComponent } from './customers/customers.component';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminHomeComponent,
+    CustomersComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
