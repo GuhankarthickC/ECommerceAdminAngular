d7fd98ebb3d48c9ef1fa52a7ea229dc7185199ca
GuhankarthickC <guhankarthick.c@gmail.com>
2022-07-10 01:42:08 +0530

    Product Component

--- Admin/src/app/products/products.component.ts
+++ Admin/src/app/products/products.component.ts
@@ -9,18 +9,27 @@ import { ProductService } from '../product.service';
 })
 export class ProductsComponent implements OnInit {
 products:Product[]=[];
+filteredproducts:Product[]=[];
 producttable:any=false;
+searchKey:string="";
+value:any;
+ischecked:any;
+grocery:Array<string>=[];
+clothing:Array<string>=[];
   constructor(private obj:ProductService) { }
 
   ngOnInit(): void {
+    this.obj.getAllProducts().subscribe(data=>{
+      this.filteredproducts=data;});
     this.get_products();
   }
   get_products():void
   {
-    this.producttable=!this.producttable;
     this.obj.getAllProducts().subscribe(data=>{
       this.products=data;
-      console.log(this.products);
+      if(this.products.length>0){
+        this.producttable=true;
+      }
     });
   }
   getbyCategory(categoryname:string):void{
@@ -28,5 +37,28 @@ producttable:any=false;
       this.products=data;
       console.log(this.products);
     });
+  }
+  search():void{
+    if(this.searchKey!="" && this.searchKey!=" ")
+    this.products=this.filteredproducts.filter((product)=>product.productName.toLowerCase().indexOf(this.searchKey.toLocaleLowerCase())!=-1);
+    else
+    this.products=this.filteredproducts;
+  }
+  price():void{
+    console.log(this.value);
+    this.products=this.filteredproducts.filter((product)=>(product.price < this.value));
+    console.log(this.products);
+  }
+  sort():void{
+    this.grocery.push("Vegetables","Fruits","Juices");
+    this.clothing.push("MenApparels","WomenApparels","Footwear");
+    if(this.ischecked){
+      this.products=this.filteredproducts.filter((product)=>(this.clothing.includes(product.category)));
+      console.log(this.products);
+    }
+    else{
+      this.products=this.filteredproducts.filter((product)=>(this.grocery.includes(product.category)));
+      console.log(this.products);
+    }
   }
 }
